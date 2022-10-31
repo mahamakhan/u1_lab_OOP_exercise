b254d9dfb148f832c161f192a4624ce8ace1f4e9
mahamakhan <mahamimranamjad@gmail.com>
2022-10-31 15:07:08 -0700

    l

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
-class School {}
+class School {
+  constructor(schoolName, level) {
+    this.schoolName = schoolName
+    this.level = level
+    this.students = []
+  }
+  enrollStudent(student) {
+    this.students.push(student)
+  }
+  listEnrollmentNumbers() {
+    return this.students.length
+  }
+}
 
-class High {}
+class High extends School {
+  constructor(schoolName) {
+    super(schoolName, 12)
 
-class Middle {}
+    this.sportsTeams = []
+  }
+  addTeam() {
+    this.sportsTeams.push(team)
+  }
+}
 
-class Primary {}
+class Middle extends School {
+  constructor(name) {
+    super(name, 6)
+  }
+}
 
-class Student {}
+class Primary extends School {
+  constructor(name, pickUpPolicy) {
+    super(name, 3)
+    this.pickUpPolicy = pickUpPolicy
+  }
+  listPickUpPolicy() {
+    return `You must pick up your child at ${this.pickUpPolicy}!`
+  }
+}
 
-class SportsTeam {}
+class Student {
+  constructor(name, email) {
+    this.name = name
+    this.email = email
+  }
+}
+
+class SportsTeam {
+  constructor(name, sport, numOfPlayers) {
+    this.name = name
+    this.sport = sport
+    this.numOfPlayers = numOfPlayers
+  }
+}
 
 module.exports = {
   School,
--- playground.js
+++ playground.js
@@ -1,2 +1,23 @@
 const { High, Middle, Primary, SportsTeam, Student } = require('.')
 // Utilize this file to create new instances of your classes. Practice using the class methods to create and add students, sportsTeams etc. Create a few schools as well.  You'll have access to your classes with the statement on line 1. DO NOT TOUCH LINE 1.
+
+const highSchool = new High()
+const student1 = new Student('John', john.com)
+const student2 = new Student('Almizan', almizan.com)
+const student3 = new Student('Hafsa', hafsa.com)
+const student4 = new Student('Aiman', aiman.com)
+const student5 = new Student('Zac', zac.com)
+
+highSchool.enrollStudent(student1)
+highSchool.enrollStudent(student2)
+highSchool.enrollStudent(student3)
+highSchool.enrollStudent(student4)
+highSchool.enrollStudent(student5)
+
+const sportsTeam1 = new SportsTeam('49ers', 'football', 30)
+const sportsTeam2 = new SportsTeam('Laykers', basketball, 20)
+
+highSchool.addTeam(sportsTeam1)
+highSchool.addTeam(sportsTeam2)
+
+console.log(highSchool)
